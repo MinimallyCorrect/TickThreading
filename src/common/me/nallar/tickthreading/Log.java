c4d4da68b958265bf7d56323bc0667f84a02583b
Ross Allan <rallanpcl@gmail.com>
2012-12-29 18:12:41 +0000

    Fix addBlockEvent, improve world names in logs.
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

--- src/common/me/nallar/tickthreading/Log.java
+++ src/common/me/nallar/tickthreading/Log.java
@@ -77,6 +77,6 @@ public class Log {
 	}
 
 	public static String name(World world) {
-		return world.getWorldInfo().getWorldName() + "/" + world.provider.getDimensionName() + "/" + world.provider.field_82913_c + (world.isRemote ? "-r" : "");
+		return world.getWorldInfo().getWorldName() + "/" + world.provider.getDimensionName() + world.getProviderName() + (world.isRemote ? "-r" : "");
 	}
 }
