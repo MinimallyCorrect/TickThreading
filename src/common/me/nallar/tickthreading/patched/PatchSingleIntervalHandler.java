88bb5b665a3ed24648fa04e0757fa9d6e4bbf587
Ross Allan <rallanpcl@gmail.com>
2013-01-08 01:56:15 +0000

    Added profiling for single intervals
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

---
+++ src/common/me/nallar/tickthreading/patched/PatchSingleIntervalHandler.java
@@ -0,0 +1,36 @@
+package me.nallar.tickthreading.patched;
+
+import java.util.EnumSet;
+
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.ITickHandler;
+import cpw.mods.fml.common.SingleIntervalHandler;
+import cpw.mods.fml.common.TickType;
+import net.minecraft.profiler.Profiler;
+
+public class PatchSingleIntervalHandler extends SingleIntervalHandler {
+	public static Profiler theProfiler = null;
+	public String name;
+
+	public PatchSingleIntervalHandler(ITickHandler handler) {
+		super(handler);
+		name = wrapped.getClass().getName().replace('.', '_');
+	}
+
+	@Override
+	public void tickStart(EnumSet<TickType> type, Object... tickData) {
+		if (theProfiler == null) {
+			theProfiler = FMLCommonHandler.instance().getMinecraftServerInstance().theProfiler;
+		}
+		theProfiler.startSection(name);
+		wrapped.tickStart(type, tickData);
+		theProfiler.endSection();
+	}
+
+	@Override
+	public void tickEnd(EnumSet<TickType> type, Object... tickData) {
+		theProfiler.startSection(name);
+		wrapped.tickEnd(type, tickData);
+		theProfiler.endSection();
+	}
+}
