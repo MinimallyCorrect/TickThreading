fbc7d59fb7a137e3603526cc76162e60abaf268f
Ross Allan <rallanpcl@gmail.com>
2013-01-12 21:08:08 +0000

    Improve concurrent world ticking performance
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

--- src/common/me/nallar/tickthreading/minecraft/patched/PatchMinecraftServer.java
+++ src/common/me/nallar/tickthreading/minecraft/patched/PatchMinecraftServer.java
@@ -1,6 +1,7 @@
 package me.nallar.tickthreading.minecraft.patched;
 
 import java.io.File;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.relauncher.Side;
@@ -20,6 +21,9 @@ import net.minecraftforge.common.DimensionManager;
 public abstract class PatchMinecraftServer extends MinecraftServer {
 	public ThreadManager threadManager;
 	private static int tickTime = 0;
+	private AtomicInteger currentWorld;
+	private Integer[] dimensionIdsToTick;
+	private Runnable tickRunnable;
 
 	public PatchMinecraftServer(File par1File) {
 		super(par1File);
@@ -80,21 +84,25 @@ public abstract class PatchMinecraftServer extends MinecraftServer {
 		int var1;
 		long startTime = System.nanoTime();
 
-		Integer[] ids = DimensionManager.getIDs();
+		dimensionIdsToTick = DimensionManager.getIDs();
 
 		if (threadManager == null) {
 			threadManager = new ThreadManager(8, "World Tick");
+			currentWorld = new AtomicInteger(0);
+			tickRunnable = new TickRunnable(this);
 		}
 
-		for (final int id : ids) {
-			if (theProfiler.profilingEnabled || !TickThreading.instance.enableWorldTickThreading || tickCounter < 100) {
-				tickWorld(id);
-			} else {
-				threadManager.run(new TickRunnable(id));
+		currentWorld.set(0);
+
+		if (tickCounter < 100 || theProfiler.profilingEnabled || !TickThreading.instance.enableWorldTickThreading) {
+			doTick();
+		} else {
+			int count = Math.min(Runtime.getRuntime().availableProcessors(), dimensionIdsToTick.length);
+			for (int i = 0; i < count; i++) {
+				threadManager.run(tickRunnable);
 			}
+			threadManager.waitForCompletion();
 		}
-
-		threadManager.waitForCompletion();
 
 		this.theProfiler.endStartSection("dim_unloading");
 		DimensionManager.unloadWorlds(worldTickTimes);
@@ -119,58 +127,59 @@ public abstract class PatchMinecraftServer extends MinecraftServer {
 	}
 
 	@Declare
-	public void tickWorld(int id) {
-		long var2 = System.nanoTime();
+	public void doTick() {
+		int i;
+		while ((i = currentWorld.getAndIncrement()) < dimensionIdsToTick.length) {
+			int id = dimensionIdsToTick[i];
+			long var2 = System.nanoTime();
 
-		if (id == 0 || this.getAllowNether()) {
-			WorldServer var4 = DimensionManager.getWorld(id);
-			this.theProfiler.startSection(var4.getWorldInfo().getWorldName());
-			this.theProfiler.startSection("pools");
-			var4.getWorldVec3Pool().clear();
-			this.theProfiler.endSection();
+			if (id == 0 || this.getAllowNether()) {
+				WorldServer var4 = DimensionManager.getWorld(id);
+				this.theProfiler.startSection(var4.getWorldInfo().getWorldName());
+				this.theProfiler.startSection("pools");
+				var4.getWorldVec3Pool().clear();
+				this.theProfiler.endSection();
 
-			if (this.tickCounter % 60 == 0) {
-				this.theProfiler.startSection("timeSync");
-				this.serverConfigManager.sendPacketToAllPlayersInDimension(new Packet4UpdateTime(var4.getTotalWorldTime(), var4.getWorldTime()), var4.provider.dimensionId);
+				if (this.tickCounter % 60 == 0) {
+					this.theProfiler.startSection("timeSync");
+					this.serverConfigManager.sendPacketToAllPlayersInDimension(new Packet4UpdateTime(var4.getTotalWorldTime(), var4.getWorldTime()), var4.provider.dimensionId);
+					this.theProfiler.endSection();
+				}
+
+				this.theProfiler.startSection("forgeTick");
+				FMLCommonHandler.instance().onPreWorldTick(var4);
+
+				CrashReport var6;
+
+				this.theProfiler.endStartSection("worldTick");
+				try {
+					var4.tick();
+				} catch (Throwable var8) {
+					var6 = CrashReport.makeCrashReport(var8, "Exception ticking world");
+					var4.addWorldInfoToCrashReport(var6);
+					throw new ReportedException(var6);
+				}
+
+				this.theProfiler.endStartSection("entityTick");
+				try {
+					var4.updateEntities();
+				} catch (Throwable var7) {
+					var6 = CrashReport.makeCrashReport(var7, "Exception ticking world entities");
+					var4.addWorldInfoToCrashReport(var6);
+					throw new ReportedException(var6);
+				}
+
+				this.theProfiler.endStartSection("postForgeTick");
+				FMLCommonHandler.instance().onPostWorldTick(var4);
+				this.theProfiler.endSection();
+				this.theProfiler.startSection("tracker");
+				var4.getEntityTracker().updateTrackedEntities();
+				this.theProfiler.endSection();
 				this.theProfiler.endSection();
 			}
 
-			this.theProfiler.startSection("forgeTick");
-			FMLCommonHandler.instance().onPreWorldTick(var4);
-
-			CrashReport var6;
-
-			this.theProfiler.endStartSection("worldTick");
-			try {
-				var4.tick();
-				if (true) {
-					throw new RuntimeException();
-				}
-			} catch (Throwable var8) {
-				var6 = CrashReport.makeCrashReport(var8, "Exception ticking world");
-				var4.addWorldInfoToCrashReport(var6);
-				throw new ReportedException(var6);
-			}
-
-			this.theProfiler.endStartSection("entityTick");
-			try {
-				var4.updateEntities();
-			} catch (Throwable var7) {
-				var6 = CrashReport.makeCrashReport(var7, "Exception ticking world entities");
-				var4.addWorldInfoToCrashReport(var6);
-				throw new ReportedException(var6);
-			}
-
-			this.theProfiler.endStartSection("postForgeTick");
-			FMLCommonHandler.instance().onPostWorldTick(var4);
-			this.theProfiler.endSection();
-			this.theProfiler.startSection("tracker");
-			var4.getEntityTracker().updateTrackedEntities();
-			this.theProfiler.endSection();
-			this.theProfiler.endSection();
+			worldTickTimes.get(id)[this.tickCounter % 100] = System.nanoTime() - var2;
 		}
-
-		worldTickTimes.get(id)[this.tickCounter % 100] = System.nanoTime() - var2;
 	}
 
 	@Declare
@@ -179,15 +188,15 @@ public abstract class PatchMinecraftServer extends MinecraftServer {
 	}
 
 	public static class TickRunnable implements Runnable {
-		final int id;
+		final MinecraftServer minecraftServer;
 
-		public TickRunnable(int id) {
-			this.id = id;
+		public TickRunnable(MinecraftServer minecraftServer) {
+			this.minecraftServer = minecraftServer;
 		}
 
 		@Override
 		public void run() {
-			mcServer.tickWorld(id);
+			minecraftServer.doTick();
 		}
 	}
 }
