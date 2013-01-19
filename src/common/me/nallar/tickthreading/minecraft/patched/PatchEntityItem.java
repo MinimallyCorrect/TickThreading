1da977028c7098645bec7aa8fc93490c723dc5d8
Ross Allan <rallanpcl@gmail.com>
2013-01-19 16:41:40 +0000

    Increase item merge radius, reduce item merge checks.
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

---
+++ src/common/me/nallar/tickthreading/minecraft/patched/PatchEntityItem.java
@@ -0,0 +1,23 @@
+package me.nallar.tickthreading.minecraft.patched;
+
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.world.World;
+
+public abstract class PatchEntityItem extends EntityItem {
+	private static final double mergeRadius = 1D;
+	private int mergeCheck = 0;
+
+	public PatchEntityItem(World par1World, double par2, double par4, double par6) {
+		super(par1World, par2, par4, par6);
+	}
+
+	@Override
+	protected void func_85054_d() {
+		if (mergeCheck++ % 2 == 0) {
+			for (Object o : this.worldObj.getEntitiesWithinAABB(EntityItem.class, this.boundingBox.expand(mergeRadius, mergeRadius, mergeRadius))) {
+				EntityItem var2 = (EntityItem) o;
+				this.combineItems(var2);
+			}
+		}
+	}
+}
