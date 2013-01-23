2b7e6a8b5c24f124e6a6d7872b5aa046ff05bf56
Ross Allan <rallanpcl@gmail.com>
2013-01-23 17:26:48 +0000

    Fix typo, add missed file
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

---
+++ src/common/me/nallar/tickthreading/minecraft/patched/PatchPlayerManagerForge.java
@@ -0,0 +1,61 @@
+package me.nallar.tickthreading.minecraft.patched;
+
+import java.util.List;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import me.nallar.tickthreading.Log;
+import me.nallar.tickthreading.patcher.Declare;
+import net.minecraft.server.management.PlayerInstance;
+import net.minecraft.server.management.PlayerManager;
+import net.minecraft.world.WorldServer;
+
+public abstract class PatchPlayerManagerForge extends PlayerManager {
+	@Declare
+	public ReentrantReadWriteLock.ReadLock playerUpdateLock_;
+	@Declare
+	public ReentrantReadWriteLock.WriteLock playersUpdateLock_;
+
+	public void construct() {
+		ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
+		playersUpdateLock = reentrantReadWriteLock.writeLock();
+		playerUpdateLock = reentrantReadWriteLock.readLock();
+	}
+
+	public PatchPlayerManagerForge(WorldServer par1WorldServer, int par2) {
+		super(par1WorldServer, par2);
+	}
+
+	@Override
+	@Declare
+	public net.minecraft.util.LongHashMap getChunkWatchers() {
+		return this.playerInstances;
+	}
+
+	@Override
+	@Declare
+	public List getChunkWatcherWithPlayers() {
+		return this.chunkWatcherWithPlayers;
+	}
+
+	@Override
+	public void updatePlayerInstances() {
+		playersUpdateLock.lock();
+		try {
+			for (Object chunkWatcherWithPlayer : this.chunkWatcherWithPlayers) {
+				if (chunkWatcherWithPlayer instanceof PlayerInstance) {
+					((PlayerInstance) chunkWatcherWithPlayer).sendChunkUpdate();
+				}
+			}
+		} catch (Exception e) {
+			Log.severe("Failed to unload some chunks", e);
+		} finally {
+			playersUpdateLock.unlock();
+		}
+
+		this.chunkWatcherWithPlayers.clear();
+
+		if (this.players.isEmpty()) {
+			this.theWorldServer.theChunkProviderServer.unloadAllChunks();
+		}
+	}
+}
