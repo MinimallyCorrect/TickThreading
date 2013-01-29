ae2230d07325ecfd135445c4976aceea8e2bd6af
Ross Allan <rallanpcl@gmail.com>
2013-01-29 14:42:33 +0000

    Better class transformation logging
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

--- src/common/me/nallar/tickthreading/minecraft/patched/PatchRelaunchClassLoader.java
+++ src/common/me/nallar/tickthreading/minecraft/patched/PatchRelaunchClassLoader.java
@@ -13,11 +13,20 @@ public abstract class PatchRelaunchClassLoader extends RelaunchClassLoader {
 
 	@Override
 	protected byte[] runTransformers(String name, byte[] basicClass) {
-		for (IClassTransformer transformer : transformers) {
-			try {
-				basicClass = transformer.transform(name, basicClass);
-			} catch (Throwable throwable) {
-				Log.severe("Failed to transform " + name, throwable);
+		if (basicClass == null) {
+			Log.warning("FML could not find the class " + name);
+		} else {
+			for (IClassTransformer transformer : transformers) {
+				try {
+					byte[] oldClass = basicClass;
+					basicClass = transformer.transform(name, basicClass);
+					if (basicClass == null) {
+						basicClass = oldClass;
+						Log.severe(transformer.getClass() + " returned a null class during transformation, ignoring.");
+					}
+				} catch (Throwable throwable) {
+					Log.severe("Failed to transform " + name, throwable);
+				}
 			}
 		}
 		return basicClass;
