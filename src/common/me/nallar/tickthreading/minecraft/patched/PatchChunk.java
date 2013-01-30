3b5111eb240ed1e75fd6e8b386f22c753b8e4eea
Ross Allan <rallanpcl@gmail.com>
2013-01-30 19:48:40 +0000

    Correct chunk logging
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

--- src/common/me/nallar/tickthreading/minecraft/patched/PatchChunk.java
+++ src/common/me/nallar/tickthreading/minecraft/patched/PatchChunk.java
@@ -48,7 +48,7 @@ public abstract class PatchChunk extends Chunk {
 		int var3 = MathHelper.floor_double(par1Entity.posZ / 16.0D);
 
 		if (var2 != this.xPosition || var3 != this.zPosition) {
-			FMLLog.warning("Entity %s added to the wrong chunk - expected x%d z%d, got x%d z%d", this.xPosition, this.zPosition, var2, var3);
+			FMLLog.warning("Entity %s added to the wrong chunk - expected x%d z%d, got x%d z%d", par1Entity.toString(), this.xPosition, this.zPosition, var2, var3);
 			return;
 		}
 
