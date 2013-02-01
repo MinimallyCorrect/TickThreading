d4b8efb635838693cf5e4d33703f025c632a314e
Ross Allan <rallanpcl@gmail.com>
2013-02-01 21:15:48 +0000

    Fix items falling through the world slowly in some cases
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

--- src/common/me/nallar/tickthreading/minecraft/patched/PatchEntityItem.java
+++ src/common/me/nallar/tickthreading/minecraft/patched/PatchEntityItem.java
@@ -22,12 +22,12 @@ public abstract class PatchEntityItem extends EntityItem {
 			--this.delayBeforeCanPickup;
 		}
 
-		boolean forceUpdate = this.ticksExisted % 75 == 0;
+		boolean forceUpdate = this.ticksExisted % 75 == 1;
 		this.prevPosX = this.posX;
 		this.prevPosY = this.posY;
 		this.prevPosZ = this.posZ;
 		this.motionY -= 0.03999999910593033D;
-		if (forceUpdate) {
+		if (forceUpdate || noClip) {
 			this.noClip = this.pushOutOfBlocks(this.posX, (this.boundingBox.minY + this.boundingBox.maxY) / 2.0D, this.posZ);
 		}
 		this.moveEntity(this.motionX, this.motionY, this.motionZ);
@@ -91,11 +91,9 @@ public abstract class PatchEntityItem extends EntityItem {
 
 	@Override
 	protected void func_85054_d() {
-		if (this.ticksExisted % 75 == 0) {
-			for (Object o : this.worldObj.getEntitiesWithinAABB(EntityItem.class, this.boundingBox.expand(mergeRadius, mergeRadius, mergeRadius))) {
-				EntityItem var2 = (EntityItem) o;
-				this.combineItems(var2);
-			}
+		for (Object o : this.worldObj.getEntitiesWithinAABB(EntityItem.class, this.boundingBox.expand(mergeRadius, mergeRadius, mergeRadius))) {
+			EntityItem var2 = (EntityItem) o;
+			this.combineItems(var2);
 		}
 	}
 }
