a9fa4a4d3cb6a12a1d3d0ba8a3223bbcda182dd4
Ross Allan <rallanpcl@gmail.com>
2013-02-02 13:01:08 +0000

    Add missed file
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

---
+++ src/common/me/nallar/patched/PatchForgeChunkManager.java
@@ -0,0 +1,15 @@
+package me.nallar.patched;
+
+import com.google.common.collect.ImmutableSetMultimap;
+
+import net.minecraft.world.ChunkCoordIntPair;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeChunkManager;
+
+public abstract class PatchForgeChunkManager extends ForgeChunkManager {
+	public static ImmutableSetMultimap<ChunkCoordIntPair, Ticket> getPersistentChunksFor(World world)
+	{
+		ImmutableSetMultimap<ChunkCoordIntPair, Ticket> forcedChunks = ForgeChunkManager.forcedChunks.get(world);
+		return forcedChunks == null ?  ImmutableSetMultimap.<ChunkCoordIntPair,Ticket>of() : forcedChunks;
+	}
+}
