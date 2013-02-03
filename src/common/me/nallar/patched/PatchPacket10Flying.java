9e54322a3f3927661b24195b57d6bd4ba0a4bb43
Ross Allan <rallanpcl@gmail.com>
2013-02-03 01:27:23 +0000

    Still show warnings if a player moves too fast and anticheat is disabled, just don't kick them
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

--- src/common/me/nallar/patched/PatchPacket10Flying.java
+++ src/common/me/nallar/patched/PatchPacket10Flying.java
@@ -18,7 +18,7 @@ public abstract class PatchPacket10Flying extends Packet10Flying {
 
 	@Override
 	public void processPacket(NetHandler par1NetHandler) {
-		if (TickThreading.instance.antiCheat && moving && yPosition != -999.0D && stance != -999.0D && par1NetHandler instanceof NetServerHandler) {
+		if (moving && yPosition != -999.0D && stance != -999.0D && par1NetHandler instanceof NetServerHandler) {
 			NetServerHandler nsh = (NetServerHandler) par1NetHandler;
 			if (nsh.teleported) {
 				nsh.teleported = false;
@@ -45,7 +45,9 @@ public abstract class PatchPacket10Flying extends Packet10Flying {
 					ServerConfigurationManager serverConfigurationManager = MinecraftServer.getServer().getConfigurationManager();
 					speed /= allowedSpeedMultiplier(entityPlayerMP);
 					if (!serverConfigurationManager.areCommandsAllowed(entityPlayerMP.username) && (averageSpeed > 50 || (!entityPlayerMP.isRiding() && averageSpeed > 20))) {
-						nsh.kickPlayerFromServer("You moved too quickly. " + TableFormatter.formatDoubleWithPrecision(averageSpeed, 3) + "m/s");
+						if (TickThreading.instance.antiCheat) {
+							nsh.kickPlayerFromServer("You moved too quickly. " + TableFormatter.formatDoubleWithPrecision(averageSpeed, 3) + "m/s");
+						}
 						Redirects.notifyAdmins(entityPlayerMP.username + " was travelling too fast: " + TableFormatter.formatDoubleWithPrecision(averageSpeed, 3) + "m/s");
 					}
 					nsh.lastPZ = this.zPosition;
