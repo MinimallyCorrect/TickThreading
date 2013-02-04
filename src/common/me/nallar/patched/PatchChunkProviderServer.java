c9b00aadd33493f9a3de09999349b4adbfee28c9
Ross Allan <rallanpcl@gmail.com>
2013-02-04 00:31:23 +0000

    Reduce CPS unload ticks
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

--- src/common/me/nallar/patched/PatchChunkProviderServer.java
+++ src/common/me/nallar/patched/PatchChunkProviderServer.java
@@ -69,16 +69,16 @@ public abstract class PatchChunkProviderServer extends ChunkProviderServer {
 	 */
 	@Override
 	public boolean unload100OldestChunks() {
-		if (worldObj.tickCount % 2 == 0 && !this.worldObj.canNotSave && !chunksToUnload.isEmpty()) {
+		if (worldObj.tickCount % 3 == 0 && !this.worldObj.canNotSave && !chunksToUnload.isEmpty()) {
 			int i = 0;
 			for (ChunkCoordIntPair forced : worldObj.getPersistentChunks().keySet()) {
 				if (this.chunksToUnload.remove(ChunkCoordIntPair.chunkXZ2Int(forced.chunkXPos, forced.chunkZPos)) && chunksToUnload.isEmpty()) {
-					i = 400;
+					i = 600;
 					break;
 				}
 			}
 
-			for (; i < 400; ++i) {
+			for (; i < 600; ++i) {
 				long var2;
 				synchronized (chunksToUnload) {
 					Iterator<Long> i$ = chunksToUnload.iterator();
