3e02f6ad32ad814c1d95aebb577903bad15d59bf
Ross Allan <rallanpcl@gmail.com>
2013-02-08 17:59:26 +0000

    Add better debugging to transferPlayerToDimension, small fix for a condition where the player entity could be killed if timing was bad.
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

--- src/common/me/nallar/patched/PatchServerConfigurationManager.java
+++ src/common/me/nallar/patched/PatchServerConfigurationManager.java
@@ -1,11 +1,18 @@
 package me.nallar.patched;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+import me.nallar.tickthreading.Log;
 import me.nallar.tickthreading.patcher.Declare;
 import me.nallar.tickthreading.util.concurrent.TwoWayReentrantReadWriteLock;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.packet.Packet41EntityEffect;
+import net.minecraft.network.packet.Packet9Respawn;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.ServerConfigurationManager;
+import net.minecraft.world.Teleporter;
+import net.minecraft.world.WorldServer;
 
 public abstract class PatchServerConfigurationManager extends ServerConfigurationManager {
 	@Declare
@@ -29,6 +36,42 @@ public abstract class PatchServerConfigurationManager extends ServerConfiguratio
 			this.playerNBTManagerObj.readPlayerData(par1EntityPlayerMP);
 		}
 		par1EntityPlayerMP.posY += 0.05;
+	}
+
+	@Override
+	public void transferPlayerToDimension(EntityPlayerMP entityPlayerMP, int toDimensionId, Teleporter teleporter) {
+		synchronized (entityPlayerMP.loadedChunks) {
+			int fromDimensionId = entityPlayerMP.dimension;
+			WorldServer fromDimension = this.mcServer.worldServerForDimension(fromDimensionId);
+			entityPlayerMP.dimension = toDimensionId;
+			WorldServer toDimension = this.mcServer.worldServerForDimension(toDimensionId);
+			if (fromDimension == toDimension) {
+				Log.severe("Can't transfer player to the dimension they are already in! " + entityPlayerMP + ", dimension: " + Log.name(toDimension));
+				return;
+			}
+			entityPlayerMP.playerNetServerHandler.sendPacketToPlayer(new Packet9Respawn(entityPlayerMP.dimension, (byte) entityPlayerMP.worldObj.difficultySetting, toDimension.getWorldInfo().getTerrainType(), toDimension.getHeight(), entityPlayerMP.theItemInWorldManager.getGameType()));
+			fromDimension.playerEntities.remove(entityPlayerMP);
+			fromDimension.loadedEntityList.remove(entityPlayerMP);
+			fromDimension.releaseEntitySkin(entityPlayerMP);
+			fromDimension.updateAllPlayersSleepingFlag();
+			int x = entityPlayerMP.chunkCoordX;
+			int z = entityPlayerMP.chunkCoordZ;
+			if (entityPlayerMP.addedToChunk && fromDimension.getChunkProvider().chunkExists(x, z)) {
+				fromDimension.getChunkFromChunkCoords(x, z).removeEntity(entityPlayerMP);
+			}
+			this.transferEntityToWorld(entityPlayerMP, fromDimensionId, fromDimension, toDimension, teleporter);
+			this.func_72375_a(entityPlayerMP, fromDimension);
+			entityPlayerMP.playerNetServerHandler.setPlayerLocation(entityPlayerMP.posX, entityPlayerMP.posY, entityPlayerMP.posZ, entityPlayerMP.rotationYaw, entityPlayerMP.rotationPitch);
+			entityPlayerMP.theItemInWorldManager.setWorld(toDimension);
+			this.updateTimeAndWeatherForPlayer(entityPlayerMP, toDimension);
+			this.syncPlayerInventory(entityPlayerMP);
+
+			for (PotionEffect potionEffect : (Iterable<PotionEffect>) entityPlayerMP.getActivePotionEffects()) {
+				entityPlayerMP.playerNetServerHandler.sendPacketToPlayer(new Packet41EntityEffect(entityPlayerMP.entityId, potionEffect));
+			}
+
+			GameRegistry.onPlayerChangedDimension(entityPlayerMP);
+		}
 	}
 
 	public PatchServerConfigurationManager(MinecraftServer par1MinecraftServer) {
