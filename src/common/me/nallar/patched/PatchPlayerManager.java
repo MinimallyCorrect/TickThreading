0b5a39cc88700d9aebfe47e79cad9a70d939e0bd
Ross Allan <rallanpcl@gmail.com>
2013-03-05 22:22:03 +0000

    Don't load chunks both normally and via async chunk IO executor on MCPC in PlayerManager/Instance.
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

--- src/common/me/nallar/patched/PatchPlayerManager.java
+++ src/common/me/nallar/patched/PatchPlayerManager.java
@@ -14,12 +14,10 @@ import net.minecraft.world.WorldServer;
 
 public abstract class PatchPlayerManager extends PlayerManager {
 	public Object chunkWatcherLock;
-	private net.minecraft.util.LongHashMap loadingPlayerInstances;
 	protected List chunkWatcherWithPlayersF;
 
 	public void construct() {
 		chunkWatcherLock = new Object();
-		loadingPlayerInstances = new net.minecraft.util.LongHashMap();
 		try {
 			chunkWatcherWithPlayersF = chunkWatcherWithPlayers;
 		} catch (NoSuchFieldError ignored) {
@@ -38,6 +36,24 @@ public abstract class PatchPlayerManager extends PlayerManager {
 		}
 	}
 
+	@Override
+	public PlayerInstance getOrCreateChunkWatcher(int par1, int par2, boolean par3) {
+		long var4 = (long) par1 + 2147483647L | (long) par2 + 2147483647L << 32;
+		PlayerInstance var6 = (PlayerInstance) this.playerInstances.getValueByKey(var4);
+
+		if (var6 == null && par3) {
+			synchronized (chunkWatcherLock) {
+				var6 = (PlayerInstance) this.playerInstances.getValueByKey(var4);
+				if (var6 == null) {
+					var6 = new PlayerInstance(this, par1, par2);
+					this.playerInstances.add(var4, var6);
+				}
+			}
+		}
+
+		return var6;
+	}
+
 	public PatchPlayerManager(WorldServer par1WorldServer, int par2) {
 		super(par1WorldServer, par2);
 	}
@@ -52,35 +68,6 @@ public abstract class PatchPlayerManager extends PlayerManager {
 	@Declare
 	public List getChunkWatcherWithPlayers() {
 		return this.chunkWatcherWithPlayersF;
-	}
-
-	@Override
-	public PlayerInstance getOrCreateChunkWatcher(int par1, int par2, boolean par3) {
-		long var4 = (long) par1 + 2147483647L | (long) par2 + 2147483647L << 32;
-		PlayerInstance var6 = (PlayerInstance) this.playerInstances.getValueByKey(var4);
-
-		if (var6 == null && (par3 || loadingPlayerInstances.containsItem(var4))) {
-			synchronized (chunkWatcherLock) {
-				var6 = (PlayerInstance) this.playerInstances.getValueByKey(var4);
-				if (var6 == null) {
-					var6 = (PlayerInstance) loadingPlayerInstances.getValueByKey(var4);
-				} else {
-					return var6;
-				}
-				if (var6 == null) {
-					var6 = new PlayerInstance(this, par1, par2);
-					this.loadingPlayerInstances.add(var4, var6);
-				}
-			}
-			getWorldServer().theChunkProviderServer.loadChunk(par1, par2);
-			synchronized (chunkWatcherLock) {
-				if (this.loadingPlayerInstances.remove(var4) != null) {
-					this.playerInstances.add(var4, var6);
-				}
-			}
-		}
-
-		return var6;
 	}
 
 	public static class QueueList implements List {
