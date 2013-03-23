5d67d8d71c546499cab97a1de8c69b455e65ae0f
Ross Allan <rallanpcl@gmail.com>
2013-03-23 23:01:09 +0000

    Revert "Improve denLib's MD5 hash implementation to be reasonably fast."
    This reverts commit b5cb1fda5eb8e3e7264010de73405c7e64fff933.

--- src/common/javassist/is/faulty/Redirects.java
+++ src/common/javassist/is/faulty/Redirects.java
@@ -1,40 +1,12 @@
 package javassist.is.faulty;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
 import me.nallar.tickthreading.Log;
 import me.nallar.tickthreading.minecraft.TickThreading;
-import me.nallar.unsafe.UnsafeUtil;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.ServerConfigurationManager;
 
 public class Redirects {
-	private static ThreadLocal<MessageDigest> messageDigestThreadLocal = new ThreadLocal<MessageDigest>() {
-		@Override
-		protected MessageDigest initialValue() {
-			try {
-				return MessageDigest.getInstance("MD-5");
-			} catch (NoSuchAlgorithmException e) {
-				throw UnsafeUtil.throwIgnoreChecked(e);
-			}
-		}
-	};
-
-	public static String md5Hash(String toHash) {
-		byte[] md5 = messageDigestThreadLocal.get().digest(toHash.getBytes());
-		StringBuilder hash = new StringBuilder();
-		for (byte b : md5) {
-			if (b >= 0) {
-				hash.append(b);
-			} else {
-				hash.append(-b);
-			}
-		}
-		return hash.toString();
-	}
-
 	public static void notifyAdmins(String message) {
 		if (!TickThreading.instance.antiCheatNotify) {
 			return;
