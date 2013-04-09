b3846b29a93b496a4b94d569c5fd19832d886ef2
Ross Allan <rallanpcl@gmail.com>
2013-04-09 16:24:03 +0100

    Remove no longer valid comment for AnvilChunkLoaderPending
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

--- src/common/me/nallar/patched/storage/PatchAnvilChunkLoaderPending.java
+++ src/common/me/nallar/patched/storage/PatchAnvilChunkLoaderPending.java
@@ -5,10 +5,6 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.ChunkCoordIntPair;
 import net.minecraft.world.chunk.storage.AnvilChunkLoaderPending;
 
-/**
- * This patch class doesn't change anything, it's just here so the prepatcher will
- * make AnvilChunkLoaderPending public
- */
 public abstract class PatchAnvilChunkLoaderPending extends AnvilChunkLoaderPending {
 	@Declare
 	public boolean unloading_;
