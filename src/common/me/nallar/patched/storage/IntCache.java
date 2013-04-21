dc88968f79fc27124cc1a0f378fa58bcb19c397c
Ross Allan <rallanpcl@gmail.com>
2013-04-21 17:20:59 +0100

    Disable IntCache - this actually reduces performance because of the synchronization, and without synchronization would still reduce performance on any modern JVM
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

---
+++ src/common/me/nallar/patched/storage/IntCache.java
@@ -0,0 +1,14 @@
+package me.nallar.patched.storage;
+
+public abstract class IntCache {
+	public static int[] a(int size) {
+		return new int[size];
+	}
+
+	public static void a() {
+	}
+
+	public static String b() {
+		return "IntCache disabled.";
+	}
+}
