0a42f278091eeab898b7a2881086ac8fbf3e7c0a
Ross Allan <rallanpcl@gmail.com>
2013-05-30 15:01:54 +0100

    Fix PatchLiquidContainerRegistry formatting.
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

--- src/common/me/nallar/patched/forge/PatchLiquidContainerRegistry.java
+++ src/common/me/nallar/patched/forge/PatchLiquidContainerRegistry.java
@@ -18,7 +18,6 @@ import net.minecraftforge.liquids.LiquidStack;
 public abstract class PatchLiquidContainerRegistry {
 	public static final int BUCKET_VOLUME = 1000;
 	public static final ItemStack EMPTY_BUCKET = new ItemStack(Item.bucketEmpty);
-
 	private static final Map<FilledItemFromLiquidKey, LiquidContainerData> mapFilledItemFromLiquid = new HashMap<FilledItemFromLiquidKey, LiquidContainerData>();
 	private static final Map<LiquidFromFilledItemKey, LiquidContainerData> mapLiquidFromFilledItem = new HashMap<LiquidFromFilledItemKey, LiquidContainerData>();
 	private static final Set<List> setContainerValidation = new HashSet<List>();
@@ -28,10 +27,9 @@ public abstract class PatchLiquidContainerRegistry {
 	/**
 	 * Default registrations
 	 */
-	static
-	{
+	static {
 		registerLiquid(new LiquidContainerData(new LiquidStack(Block.waterStill, LiquidContainerRegistry.BUCKET_VOLUME), new ItemStack(Item.bucketWater), new ItemStack(Item.bucketEmpty)));
-		registerLiquid(new LiquidContainerData(new LiquidStack(Block.lavaStill, LiquidContainerRegistry.BUCKET_VOLUME),  new ItemStack(Item.bucketLava), new ItemStack(Item.bucketEmpty)));
+		registerLiquid(new LiquidContainerData(new LiquidStack(Block.lavaStill, LiquidContainerRegistry.BUCKET_VOLUME), new ItemStack(Item.bucketLava), new ItemStack(Item.bucketEmpty)));
 		registerLiquid(new LiquidContainerData(new LiquidStack(Block.waterStill, LiquidContainerRegistry.BUCKET_VOLUME), new ItemStack(Item.potion), new ItemStack(Item.glassBottle)));
 		// registerLiquid(new LiquidContainerData(new LiquidStack(Item.bucketMilk, LiquidContainerRegistry.BUCKET_VOLUME), new ItemStack(Item.bucketMilk), new ItemStack(Item.bucketEmpty)));
 	}
@@ -107,8 +105,7 @@ public abstract class PatchLiquidContainerRegistry {
 	/**
 	 * To register a container with a non-bucket size, the LiquidContainerData entry simply needs to use a size other than LiquidManager.BUCKET_VOLUME
 	 */
-	public static synchronized void registerLiquid(LiquidContainerData data)
-	{
+	public static synchronized void registerLiquid(LiquidContainerData data) {
 		ItemStack container = data.container;
 		LiquidStack liquid = data.stillLiquid;
 		mapFilledItemFromLiquid.put(new FilledItemFromLiquidKey(container.itemID, container.getItemDamage(), liquid.itemID, liquid.itemMeta), data);
@@ -119,10 +116,8 @@ public abstract class PatchLiquidContainerRegistry {
 		liquids.add(data);
 	}
 
-	public static LiquidStack getLiquidForFilledItem(ItemStack filledContainer)
-	{
-		if (filledContainer == null)
-		{
+	public static LiquidStack getLiquidForFilledItem(ItemStack filledContainer) {
+		if (filledContainer == null) {
 			return null;
 		}
 
@@ -130,27 +125,22 @@ public abstract class PatchLiquidContainerRegistry {
 		return ret == null ? null : ret.stillLiquid.copy();
 	}
 
-	public static ItemStack fillLiquidContainer(LiquidStack liquid, ItemStack emptyContainer)
-	{
-		if (emptyContainer == null || liquid == null)
-		{
+	public static ItemStack fillLiquidContainer(LiquidStack liquid, ItemStack emptyContainer) {
+		if (emptyContainer == null || liquid == null) {
 			return null;
 		}
 
 		LiquidContainerData ret = mapFilledItemFromLiquid.get(new FilledItemFromLiquidKey(emptyContainer.itemID, emptyContainer.getItemDamage(), liquid.itemID, liquid.itemMeta));
 
-		if (ret != null && liquid.amount >= ret.stillLiquid.amount)
-		{
+		if (ret != null && liquid.amount >= ret.stillLiquid.amount) {
 			return ret.filled.copy();
 		}
 
 		return null;
 	}
 
-	public static boolean containsLiquid(ItemStack filledContainer, LiquidStack liquid)
-	{
-		if (filledContainer == null || liquid == null)
-		{
+	public static boolean containsLiquid(ItemStack filledContainer, LiquidStack liquid) {
+		if (filledContainer == null || liquid == null) {
 			return false;
 		}
 
@@ -159,15 +149,12 @@ public abstract class PatchLiquidContainerRegistry {
 		return ret != null && ret.stillLiquid.isLiquidEqual(liquid);
 	}
 
-	public static boolean isBucket(ItemStack container)
-	{
-		if (container == null)
-		{
+	public static boolean isBucket(ItemStack container) {
+		if (container == null) {
 			return false;
 		}
 
-		if (container.isItemEqual(EMPTY_BUCKET))
-		{
+		if (container.isItemEqual(EMPTY_BUCKET)) {
 			return true;
 		}
 
@@ -175,28 +162,23 @@ public abstract class PatchLiquidContainerRegistry {
 		return ret != null && ret.container.isItemEqual(EMPTY_BUCKET);
 	}
 
-	public static boolean isContainer(ItemStack container)
-	{
+	public static boolean isContainer(ItemStack container) {
 		return isEmptyContainer(container) || isFilledContainer(container);
 	}
 
-	public static boolean isEmptyContainer(ItemStack emptyContainer)
-	{
+	public static boolean isEmptyContainer(ItemStack emptyContainer) {
 		return emptyContainer != null && setContainerValidation.contains(Arrays.asList(emptyContainer.itemID, emptyContainer.getItemDamage()));
 	}
 
-	public static boolean isFilledContainer(ItemStack filledContainer)
-	{
+	public static boolean isFilledContainer(ItemStack filledContainer) {
 		return filledContainer != null && getLiquidForFilledItem(filledContainer) != null;
 	}
 
-	public static boolean isLiquid(ItemStack item)
-	{
+	public static boolean isLiquid(ItemStack item) {
 		return item != null && setLiquidValidation.contains(Arrays.asList(item.itemID, item.getItemDamage()));
 	}
 
-	public static LiquidContainerData[] getRegisteredLiquidContainerData()
-	{
+	public static LiquidContainerData[] getRegisteredLiquidContainerData() {
 		return liquids.toArray(new LiquidContainerData[liquids.size()]);
 	}
 }
