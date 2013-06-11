febf6307853a638d3348255ebcc2e86befcd16e2
Ross Allan <rallanpcl@gmail.com>
2013-06-11 19:11:14 +0100

    Improve performance of NBTTagCompound.equals, one last time.
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

--- src/common/me/nallar/patched/nbt/PatchNBTTagCompound.java
+++ src/common/me/nallar/patched/nbt/PatchNBTTagCompound.java
@@ -20,9 +20,9 @@ import net.minecraft.util.ReportedException;
 public abstract class PatchNBTTagCompound extends NBTTagCompound {
 	@Override
 	public synchronized NBTBase copy() {
-		NBTTagCompound var1 = new NBTTagCompound(this.getName());
+		NBTTagCompound var1 = new NBTTagCompound(getName());
 
-		for (Map.Entry<String, NBTBase> o : ((Map<String, NBTBase>) this.tagMap).entrySet()) {
+		for (Map.Entry<String, NBTBase> o : ((Map<String, NBTBase>) tagMap).entrySet()) {
 			var1.setTag(o.getKey(), o.getValue().copy());
 		}
 
@@ -35,7 +35,7 @@ public abstract class PatchNBTTagCompound extends NBTTagCompound {
 			NBTTagByte nbtTagByte = (NBTTagByte) tagMap.get(tag);
 			return nbtTagByte == null ? 0 : nbtTagByte.data;
 		} catch (ClassCastException e) {
-			throw new ReportedException(this.createCrashReport(tag, 1, e));
+			throw new ReportedException(createCrashReport(tag, 1, e));
 		}
 	}
 
@@ -45,7 +45,7 @@ public abstract class PatchNBTTagCompound extends NBTTagCompound {
 			NBTTagShort nbtTagShort = (NBTTagShort) tagMap.get(tag);
 			return nbtTagShort == null ? 0 : nbtTagShort.data;
 		} catch (ClassCastException e) {
-			throw new ReportedException(this.createCrashReport(tag, 2, e));
+			throw new ReportedException(createCrashReport(tag, 2, e));
 		}
 	}
 
@@ -55,7 +55,7 @@ public abstract class PatchNBTTagCompound extends NBTTagCompound {
 			NBTTagInt nbtTagInt = (NBTTagInt) tagMap.get(tag);
 			return nbtTagInt == null ? 0 : nbtTagInt.data;
 		} catch (ClassCastException e) {
-			throw new ReportedException(this.createCrashReport(tag, 3, e));
+			throw new ReportedException(createCrashReport(tag, 3, e));
 		}
 	}
 
@@ -65,7 +65,7 @@ public abstract class PatchNBTTagCompound extends NBTTagCompound {
 			NBTTagLong nbtTagLong = (NBTTagLong) tagMap.get(tag);
 			return nbtTagLong == null ? 0 : nbtTagLong.data;
 		} catch (ClassCastException e) {
-			throw new ReportedException(this.createCrashReport(tag, 4, e));
+			throw new ReportedException(createCrashReport(tag, 4, e));
 		}
 	}
 
@@ -75,7 +75,7 @@ public abstract class PatchNBTTagCompound extends NBTTagCompound {
 			NBTTagFloat nbtTagFloat = (NBTTagFloat) tagMap.get(tag);
 			return nbtTagFloat == null ? 0 : nbtTagFloat.data;
 		} catch (ClassCastException e) {
-			throw new ReportedException(this.createCrashReport(tag, 5, e));
+			throw new ReportedException(createCrashReport(tag, 5, e));
 		}
 	}
 
@@ -85,7 +85,7 @@ public abstract class PatchNBTTagCompound extends NBTTagCompound {
 			NBTTagDouble nbtTagDouble = (NBTTagDouble) tagMap.get(tag);
 			return nbtTagDouble == null ? 0 : nbtTagDouble.data;
 		} catch (ClassCastException e) {
-			throw new ReportedException(this.createCrashReport(tag, 6, e));
+			throw new ReportedException(createCrashReport(tag, 6, e));
 		}
 	}
 
@@ -95,7 +95,7 @@ public abstract class PatchNBTTagCompound extends NBTTagCompound {
 			NBTTagString nbtTagString = (NBTTagString) tagMap.get(tag);
 			return nbtTagString == null ? "" : nbtTagString.data;
 		} catch (ClassCastException e) {
-			throw new ReportedException(this.createCrashReport(tag, 8, e));
+			throw new ReportedException(createCrashReport(tag, 8, e));
 		}
 	}
 
@@ -105,7 +105,7 @@ public abstract class PatchNBTTagCompound extends NBTTagCompound {
 			NBTTagByteArray nbtTagByteArray = (NBTTagByteArray) tagMap.get(tag);
 			return nbtTagByteArray == null ? new byte[0] : nbtTagByteArray.byteArray;
 		} catch (ClassCastException e) {
-			throw new ReportedException(this.createCrashReport(tag, 7, e));
+			throw new ReportedException(createCrashReport(tag, 7, e));
 		}
 	}
 
@@ -115,7 +115,7 @@ public abstract class PatchNBTTagCompound extends NBTTagCompound {
 			NBTTagIntArray nbtTagIntArray = (NBTTagIntArray) tagMap.get(tag);
 			return nbtTagIntArray == null ? new int[0] : nbtTagIntArray.intArray;
 		} catch (ClassCastException e) {
-			throw new ReportedException(this.createCrashReport(tag, 11, e));
+			throw new ReportedException(createCrashReport(tag, 11, e));
 		}
 	}
 
@@ -125,7 +125,7 @@ public abstract class PatchNBTTagCompound extends NBTTagCompound {
 			NBTTagCompound nbtTagCompound = (NBTTagCompound) tagMap.get(tag);
 			return nbtTagCompound == null ? new NBTTagCompound(tag) : nbtTagCompound;
 		} catch (ClassCastException e) {
-			throw new ReportedException(this.createCrashReport(tag, 10, e));
+			throw new ReportedException(createCrashReport(tag, 10, e));
 		}
 	}
 
@@ -135,7 +135,7 @@ public abstract class PatchNBTTagCompound extends NBTTagCompound {
 			NBTTagList nbtTagList = (NBTTagList) tagMap.get(tag);
 			return nbtTagList == null ? new NBTTagList(tag) : nbtTagList;
 		} catch (ClassCastException e) {
-			throw new ReportedException(this.createCrashReport(tag, 9, e));
+			throw new ReportedException(createCrashReport(tag, 9, e));
 		}
 	}
 
@@ -151,7 +151,8 @@ public abstract class PatchNBTTagCompound extends NBTTagCompound {
 			return true;
 		} else if (o instanceof NBTTagCompound) {
 			NBTTagCompound other = (NBTTagCompound) o;
-			if (this.getId() == other.getId() && ((this.getRawName() != null || other.getRawName() == null) && (this.getRawName() == null || other.getRawName() != null) && (this.getRawName() == null || this.getRawName().equals(other.getRawName())))) {
+			String name;
+			if (getId() == other.getId() && ((name = getRawName()) == null || name.equals(other.getRawName()))) {
 				Map map = tagMap;
 				Map otherMap = other.getTagMap();
 				if (map == otherMap) {
