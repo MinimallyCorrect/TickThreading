4f24484cf8f37fe4f0be3c66dde2dd27d95c4178
Ross Allan <rallanpcl@gmail.com>
2013-06-30 02:11:32 +0100

    Make PatchTcpReaderThread abstract
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

--- src/common/nallar/patched/network/PatchTcpReaderThread.java
+++ src/common/nallar/patched/network/PatchTcpReaderThread.java
@@ -3,7 +3,7 @@ package nallar.patched.network;
 import net.minecraft.network.TcpConnection;
 import net.minecraft.network.TcpReaderThread;
 
-public class PatchTcpReaderThread extends TcpReaderThread {
+public abstract class PatchTcpReaderThread extends TcpReaderThread {
 	PatchTcpReaderThread(final TcpConnection par1TcpConnection, final String par2Str) {
 		super(par1TcpConnection, par2Str);
 	}
