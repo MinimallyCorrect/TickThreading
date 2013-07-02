660025da3e78de02a4dba3c7da108054febd0333
Ross Allan <rallanpcl@gmail.com>
2013-07-02 02:40:01 +0100

    More logging for #833
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

--- src/common/nallar/tickthreading/util/contextaccess/ContextAccessProvider.java
+++ src/common/nallar/tickthreading/util/contextaccess/ContextAccessProvider.java
@@ -14,11 +14,19 @@ class ContextAccessProvider {
 				ContextAccess contextAccess = (ContextAccess) clazz.newInstance();
 				Class<?> currentClass = contextAccess.getContext(0);
 				if (currentClass != ContextAccessProvider.class) {
-					throw new Error("Wrong class returned: " + currentClass + ", expected ContextAccessProvider");
+					StringBuilder sb = new StringBuilder();
+					sb.append("Stack:\n");
+					for (int i = -2; i < 3; i++) {
+						try {
+							sb.append(contextAccess.getContext(i).getName()).append(" at ").append(i);
+						} catch (Throwable ignored) {
+						}
+					}
+					throw new Error("Wrong class returned: " + currentClass + ", expected ContextAccessProvider. " + sb);
 				}
 				return contextAccess;
 			} catch (Throwable t) {
-				Log.warning("Failed to instantiate " + clazz, t);
+				Log.warning("Unable to set up context access class " + clazz, t);
 			}
 		}
 		throw new Error("Failed to set up any context access");
