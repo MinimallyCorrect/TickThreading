MinimallyCorrect/TickThreading

commit d6bfc829b05d377e1cf9069abe4d421f42e643c2

Add new prepatcher annotation @ExposeInner, used to make an inner class of the class which is being extended by a patch class public.
Signed-off-by: Ross Allan <rallanpcl@gmail.com>

src/main/java/nallar/patched/forge/PatchForgeChunkManager.java
@@ -10,6 +10,7 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Loader;
+import nallar.patched.annotation.ExposeInner;
 import nallar.patched.annotation.Public;
 import nallar.tickthreading.patcher.Declare;
 import net.minecraft.entity.Entity;
@@ -31,6 +32,7 @@ import java.util.logging.*;
 
 @SuppressWarnings("UnusedDeclaration")
 @Public
+@ExposeInner("Ticket")
 public abstract class PatchForgeChunkManager extends ForgeChunkManager {
 	private static final Map<World, ArrayListMultimap<String, Ticket>> worldLoadedTickets = new HashMap<World, ArrayListMultimap<String, Ticket>>();
 	private static final Map<World, Map<String, ListMultimap<String, Ticket>>> worldPlayerLoadedTickets = new HashMap<World, Map<String, ListMultimap<String, Ticket>>>();
@@ -87,7 +89,7 @@ public abstract class PatchForgeChunkManager extends ForgeChunkManager {
 		ticket.world.getChunkProvider().loadChunk(chunk.chunkXPos, chunk.chunkZPos);
 	}
 
-	static void saveWorld(World world) {
+	public static void saveWorld(World world) {
 		// only persist persistent worlds
 		if (!(world instanceof WorldServer)) {
 			return;
@@ -142,7 +144,7 @@ public abstract class PatchForgeChunkManager extends ForgeChunkManager {
 		}
 	}
 
-	static void loadWorld(World world) {
+	public static void loadWorld(World world) {
 		ArrayListMultimap<String, Ticket> newTickets = ArrayListMultimap.create();
 		tickets.put(world, newTickets);
 
@@ -196,10 +198,11 @@ public abstract class PatchForgeChunkManager extends ForgeChunkManager {
 					}
 					if (ticket.hasKey("Player")) {
 						tick.player = ticket.getString("Player");
-						if (!playerLoadedTickets.containsKey(tick.modId)) {
-							playerLoadedTickets.put(modId, ArrayListMultimap.<String, Ticket>create());
+						ArrayListMultimap<String, Ticket> playerLoadedTicket = playerLoadedTickets.get(tick.modId);
+						if (playerLoadedTicket == null) {
+							playerLoadedTickets.put(modId, playerLoadedTicket = ArrayListMultimap.<String, Ticket>create());
 						}
-						playerLoadedTickets.get(tick.modId).put(tick.player, tick);
+						playerLoadedTicket.put(tick.player, tick);
 					} else {
 						loadedTickets.put(modId, tick);
 					}
@@ -278,7 +281,7 @@ public abstract class PatchForgeChunkManager extends ForgeChunkManager {
 		}
 	}
 
-	static void unloadWorld(World world) {
+	public static void unloadWorld(World world) {
 		// World save fires before this event so the chunk loading info will be done
 		if (!(world instanceof WorldServer)) {
 			return;

buildSrc/src/nallar/nmsprepatcher/PrePatcher.java
@@ -32,6 +32,7 @@ class PrePatcher {
 	private static final Pattern packageFieldPattern = Pattern.compile("\n    ? ?([^ ]+  ? ?[^ ]+);");
 	private static final Pattern innerClassPattern = Pattern.compile("[^\n]public (?:static )?class ([^ \n]+)[ \n]", Pattern.MULTILINE);
 	private static final Pattern importPattern = Pattern.compile("\nimport ([^;]+?);", Pattern.MULTILINE | Pattern.DOTALL);
+	private static final Pattern exposeInnerPattern = Pattern.compile("\n@ExposeInner\\(\"([^\"]+)\"\\)", Pattern.MULTILINE | Pattern.DOTALL);
 	private static final Splitter spaceSplitter = Splitter.on(' ').omitEmptyStrings();
 	private static final Splitter commaSplitter = Splitter.on(',').omitEmptyStrings().trimResults();
 	private static final Map<String, PatchInfo> patchClasses = new HashMap<String, PatchInfo>();
@@ -86,12 +87,12 @@ class PrePatcher {
 			log.warning("Unable to find class " + shortClassName + " for " + file);
 			return;
 		}
-		PatchInfo patchInfo = patchClasses.get(className);
-		if (patchInfo == null) {
-			patchInfo = new PatchInfo();
-			patchClasses.put(className, patchInfo);
+		Matcher exposeInnerMatcher = exposeInnerPattern.matcher(contents);
+		while (exposeInnerMatcher.find()) {
+			log.severe("Inner class name: " + className + "$" + exposeInnerMatcher.group(1));
+			getOrMakePatchInfo(className + "$" + exposeInnerMatcher.group(1), shortClassName + "$" + exposeInnerMatcher.group(1)).makePublic = true;
 		}
-		patchInfo.shortClassName = shortClassName;
+		PatchInfo patchInfo = getOrMakePatchInfo(className, shortClassName);
 		Matcher matcher = declareMethodPattern.matcher(contents);
 		while (matcher.find()) {
 			Matcher methodInfoMatcher = methodInfoPattern.matcher(matcher.group(1));
@@ -193,6 +194,16 @@ class PrePatcher {
 		if (contents.contains("\n@Public")) {
 			patchInfo.makePublic = true;
 		}
+	}
+
+	private static PatchInfo getOrMakePatchInfo(String className, String shortClassName) {
+		PatchInfo patchInfo = patchClasses.get(className);
+		if (patchInfo == null) {
+			patchInfo = new PatchInfo();
+			patchClasses.put(className, patchInfo);
+		}
+		patchInfo.shortClassName = shortClassName;
+		return patchInfo;
 	}
 
 	private static int accessStringToInt(String access) {
@@ -568,6 +579,10 @@ class PrePatcher {
 
 	public static byte[] patchCode(byte[] inputCode, String inputClassName) {
 		PatchInfo patchInfo = patchForClass(inputClassName);
+		if (inputClassName.contains("Ticket")) {
+			log.severe("ticket found: " + inputClassName);
+			log.severe(String.valueOf(patchInfo));
+		}
 		if (patchInfo == null) {
 			return inputCode;
 		}

src/main/java/nallar/patched/annotation/ExposeInner.java
@@ -1,0 +1,8 @@
+package nallar.patched.annotation;
+
+/**
+ * Makes an inner class (name must be specified) of the class which is being extended public
+ */
+public @interface ExposeInner {
+	String value();
+}

src/main/java/nallar/patched/forge/PatchLoader.java
@@ -11,11 +11,13 @@ import cpw.mods.fml.common.DuplicateModsFoundException;
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.ModContainer;
+import nallar.patched.annotation.ExposeInner;
 import nallar.tickthreading.patcher.Declare;
 
 import java.io.*;
 import java.util.*;
 
+@ExposeInner("ModIdComparator")
 public abstract class PatchLoader extends Loader {
 	public static boolean isModLoaded(String modname) {
 		return instance().isModLoadedFast(modname);

commit 8f41321e56942260b52f31d091b951d7020b32c0

Fix wrong variable type in PatchForgeChunkManager introduced in d6bfc829b05d3 - oops.
Signed-off-by: Ross Allan <rallanpcl@gmail.com>

src/main/java/nallar/patched/forge/PatchForgeChunkManager.java
@@ -198,7 +198,7 @@ public abstract class PatchForgeChunkManager extends ForgeChunkManager {
 					}
 					if (ticket.hasKey("Player")) {
 						tick.player = ticket.getString("Player");
-						ArrayListMultimap<String, Ticket> playerLoadedTicket = playerLoadedTickets.get(tick.modId);
+						ListMultimap<String, Ticket> playerLoadedTicket = playerLoadedTickets.get(tick.modId);
 						if (playerLoadedTicket == null) {
 							playerLoadedTickets.put(modId, playerLoadedTicket = ArrayListMultimap.<String, Ticket>create());
 						}