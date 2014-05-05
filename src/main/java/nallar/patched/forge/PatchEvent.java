617655674d85a0c5734ed58db09a61bdf7f562a4
Ross Allan <rallanpcl@gmail.com>
2014-05-05 19:09:39 +0100

    Update PatchEvent for Forge changes - no longer ignores the annotation parameter of hasAnnotation.
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

--- src/main/java/nallar/patched/forge/PatchEvent.java
+++ src/main/java/nallar/patched/forge/PatchEvent.java
@@ -1,29 +1,32 @@
 package nallar.patched.forge;
 
 import nallar.collections.PartiallySynchronizedMap;
-import net.minecraftforge.event.Cancelable;
 import net.minecraftforge.event.Event;
 
 public abstract class PatchEvent extends Event {
-	private static final PartiallySynchronizedMap<Class, Boolean> annotationMap = new PartiallySynchronizedMap<Class, Boolean>();
+	private static final PartiallySynchronizedMap<Class, PartiallySynchronizedMap<Class, Boolean>> annotationMap = new PartiallySynchronizedMap<Class, PartiallySynchronizedMap<Class, Boolean>>();
 
 	@Override
 	protected boolean hasAnnotation(Class annotation) {
 		Class cls = this.getClass();
-		Boolean cachedResult = annotationMap.get(cls);
+		PartiallySynchronizedMap<Class, Boolean> map = annotationMap.get(cls);
+		if (map == null) {
+			map = new PartiallySynchronizedMap<Class, Boolean>();
+			annotationMap.put(cls, map);
+		}
+		Boolean cachedResult = map.get(annotation);
 		if (cachedResult != null) {
 			return cachedResult;
 		}
 		Class searchClass = cls;
 		while (searchClass != Event.class) {
-			if (searchClass.isAnnotationPresent(Cancelable.class)) // TODO: Forge bug, not fixed. Buggy behaviour may be required for some mods.
-			{
-				annotationMap.put(cls, true);
+			if (searchClass.isAnnotationPresent(annotation)) {
+				map.put(annotation, true);
 				return true;
 			}
 			searchClass = searchClass.getSuperclass();
 		}
-		annotationMap.put(cls, false);
+		map.put(annotation, false);
 		return false;
 	}
 }
