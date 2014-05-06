f926cb705a6ea0f83731553a6c72433aba3fb21d
Ross Allan <rallanpcl@gmail.com>
2014-05-06 20:27:48 +0100

    Document ContextAccess.getContext(int)
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

--- src/main/java/nallar/tickthreading/util/contextaccess/ContextAccess.java
+++ src/main/java/nallar/tickthreading/util/contextaccess/ContextAccess.java
@@ -3,6 +3,11 @@ package nallar.tickthreading.util.contextaccess;
 public interface ContextAccess {
 	public static final ContextAccess $ = ContextAccessProvider.getContextAccess();
 
+	/**
+	 *
+	 * @param depth Depth of class to get - 0 = method you're calling this from, 1 = caller of the method calling getContext, 2 = caller's caller etc.
+	 * @return Class object for context
+	 */
 	Class getContext(int depth);
 
 	boolean runningUnder(Class c);
