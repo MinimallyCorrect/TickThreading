40aa6422dccaecc3855dce9c40d11fa11a0f28c5
Ross Allan <rallanpcl@gmail.com>
2014-05-07 13:25:13 +0100

    Fix #1076
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

--- src/main/java/cpw/mods/fml/relauncher/FMLLogFormatter.java
+++
@@ -1,9 +0,0 @@
-package cpw.mods.fml.relauncher;
-
-import nallar.log.LogFormatter;
-
-@SuppressWarnings("EmptyClass")
-public class FMLLogFormatter extends LogFormatter {
-	// This class would be loaded before TT's classloader can init as it is required for FML's logging
-	// This replaces the FML version with one that delegates to mine.
-}
