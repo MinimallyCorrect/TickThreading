c554218fb09f50e884c246478820fc3cf511c18b
Ross Allan <ross@nallar.me>
2016-02-17 23:54:32 +0000

    Add MixinMinecraftServer. Add tasks queue to MixinWorld.

--- src/main/java/nallar/tickthreading/mixin/MixinWorld.java
+++ src/main/java/nallar/tickthreading/mixin/MixinWorld.java
@@ -2,21 +2,27 @@ package nallar.tickthreading.mixin;
 
 import me.nallar.mixin.Add;
 import me.nallar.mixin.Mixin;
-import net.minecraft.profiler.Profiler;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldProvider;
-import net.minecraft.world.storage.ISaveHandler;
-import net.minecraft.world.storage.WorldInfo;
+
+import java.util.*;
 
 @Mixin
 public abstract class MixinWorld extends World {
+	@Add
+	private final ArrayDeque<Runnable> tasks_ = null;
 	@Add
 	public boolean unloaded_;
 	@Add
 	private String cachedName_;
 
-	protected MixinWorld(ISaveHandler saveHandlerIn, WorldInfo info, WorldProvider providerIn, Profiler profilerIn, boolean client) {
-		super(saveHandlerIn, info, providerIn, profilerIn, client);
+	protected MixinWorld() {
+		super(null, null, null, null, false);
+		tasks = new ArrayDeque<>();
+	}
+
+	@Override
+	public void updateEntities() {
+		// TODO: Copy from old TT, merge changes in MC, run from tasks list
 	}
 
 	@Add
---
+++ src/main/java/nallar/tickthreading/mixin/server/MixinMinecraftServer.java
@@ -0,0 +1,26 @@
+package nallar.tickthreading.mixin.server;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import me.nallar.mixin.Mixin;
+import net.minecraft.server.MinecraftServer;
+
+import java.util.concurrent.*;
+
+@Mixin
+public abstract class MixinMinecraftServer extends MinecraftServer {
+	public MixinMinecraftServer() {
+		super(null, null);
+	}
+
+	@Override
+	public <V> ListenableFuture<V> callFromMainThread(Callable<V> callable) {
+		// TODO: Replace this with error, move all scheduled tasks to be per world?
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public ListenableFuture<Object> addScheduledTask(Runnable runnableToSchedule) {
+		// TODO: Replace this with error, move all scheduled tasks to be per world?
+		throw new UnsupportedOperationException();
+	}
+}
