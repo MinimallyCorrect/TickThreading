451a475f2cc67ea9998895d8edc5e23ec24598d0
Ross Allan <ross@nallar.me>
2017-05-08 15:27:35 +0100

    Remove Metrics, no longer used. Add Signal handler to shut down server cleanly on SIGTERM/SIGINT/SIGHUP.

--- src/main/java/nallar/tickthreading/mod/TickThreading.java
+++ src/main/java/nallar/tickthreading/mod/TickThreading.java
@@ -1,12 +1,37 @@
 package nallar.tickthreading.mod;
 
-import nallar.tickthreading.reporting.Metrics;
-import nallar.tickthreading.util.Version;
+import lombok.val;
+import nallar.tickthreading.log.Log;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
 
 @Mod(modid = "@MOD_ID@", version = "@MOD_VERSION@", name = "@MOD_NAME@", acceptableRemoteVersions = "*", acceptedMinecraftVersions = "[@MC_VERSION@]")
 public class TickThreading {
-	static {
-		new Metrics("@MOD_NAME@", Version.VERSION);
+	private static void handleSignal(String name, SignalHandler handler) {
+		try {
+			Signal.handle(new Signal(name), handler);
+		} catch (IllegalArgumentException ignored) {
+		}
+	}
+
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event) {
+		SignalHandler handler = signal -> {
+			Log.info("Received signal " + signal.getName() + ". Stopping server.");
+			val server = FMLCommonHandler.instance().getMinecraftServerInstance();
+			server.initiateShutdown();
+			while (!server.isServerStopped()) {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException ignored) {
+				}
+			}
+		};
+		handleSignal("TERM", handler);
+		handleSignal("INT", handler);
+		handleSignal("HUP", handler);
 	}
 }
