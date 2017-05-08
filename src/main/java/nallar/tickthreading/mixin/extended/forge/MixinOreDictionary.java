bc242dd1cefc5f4cf1f5953b26829eafec266825
Ross Allan <ross@nallar.me>
2017-05-08 01:09:52 +0100

    Avoid pointless overly frequent chunk saving

---
+++ src/main/java/nallar/tickthreading/mixin/extended/forge/MixinOreDictionary.java
@@ -0,0 +1,53 @@
+package nallar.tickthreading.mixin.extended.forge;
+
+import lombok.val;
+import me.nallar.mixin.Mixin;
+import me.nallar.mixin.OverrideStatic;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.FMLLog;
+import net.minecraftforge.fml.common.registry.GameData;
+import net.minecraftforge.oredict.OreDictionary;
+import org.apache.logging.log4j.Level;
+
+import java.util.*;
+
+@Mixin
+public abstract class MixinOreDictionary extends OreDictionary {
+	@OverrideStatic
+	public static int[] getOreIDs(ItemStack stack) {
+		if (stack == null)
+			throw new IllegalArgumentException("Stack can not be null!");
+
+		val item = stack.getItem();
+		if (item == null)
+			throw new IllegalArgumentException("Stack can not be null!");
+
+		val registryName = item.delegate.name();
+		if (registryName == null) {
+			FMLLog.log(Level.DEBUG, "Attempted to find the oreIDs for an unregistered object (%s). This won't work very well.", new Object[]{stack});
+			return new int[0];
+		}
+
+		// TODO: cache by registryName. Need to add ability for static constructors
+
+		Set<Integer> set = new HashSet<>();
+		int id = GameData.getItemRegistry().getId(registryName);
+		List<Integer> ids = stackToId.get(id);
+		if (ids != null) {
+			set.addAll(ids);
+		}
+
+		ids = stackToId.get(id | stack.getItemDamage() + 1 << 16);
+		if (ids != null) {
+			set.addAll(ids);
+		}
+
+		val tmp = set.toArray(new Integer[0]);
+		val ret = new int[tmp.length];
+		for (int x = 0; x < tmp.length; ++x) {
+			ret[x] = tmp[x];
+		}
+
+		return ret;
+	}
+}
---
+++ src/main/java/nallar/tickthreading/mixin/world/MixinChunk.java
@@ -0,0 +1,20 @@
+package nallar.tickthreading.mixin.world;
+
+import me.nallar.mixin.Mixin;
+import net.minecraft.world.chunk.Chunk;
+
+@Mixin
+public abstract class MixinChunk extends Chunk {
+	public MixinChunk() {
+		super(null, 0, 0);
+	}
+
+	@Override
+	public boolean needsSaving(boolean force) {
+		if (force) {
+			return isModified || (hasEntities && this.worldObj.getTotalWorldTime() != this.lastSaveTime);
+		}
+
+		return (isModified || hasEntities) && this.worldObj.getTotalWorldTime() >= this.lastSaveTime + 1200L;
+	}
+}
