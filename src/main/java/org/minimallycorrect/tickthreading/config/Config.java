ae2da2d0d3c1f930ac9afddd4d2bc9628f302cfd
Ross Allan <ross@nallar.me>
2017-08-06 17:09:13 +0100

    Add separatePerWorldTickLoops option to Config

--- src/main/java/org/minimallycorrect/tickthreading/config/Config.java
+++ src/main/java/org/minimallycorrect/tickthreading/config/Config.java
@@ -20,6 +20,8 @@ public class Config {
 	public boolean worldUnloading = true;
 	@Entry(description = "Enable cleaning of unloaded worlds to aid in determining the cause of world leaks")
 	public boolean worldCleaning = true;
+	@Entry(description = "[extended] Separate per-world tick loops. When enabled each world's main loop is decoupled from other worlds. When disabled, the slowest world will reduce the TPS of all worlds.")
+	public boolean separatePerWorldTickLoops = true;
 
 	private static Config loadConfig() {
 		val typedConfig = TypedConfig.of(Config.class, Paths.get("config", "tickthreading", "tickthreading.cfg"));
