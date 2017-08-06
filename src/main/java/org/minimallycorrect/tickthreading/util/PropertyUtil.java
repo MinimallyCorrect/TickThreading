bcae36ab24b064e607598a9d173f253569b183b6
Ross Allan <ross@nallar.me>
2017-08-06 17:11:25 +0100

    Add PropertyUtil.get for ints

--- src/main/java/org/minimallycorrect/tickthreading/util/PropertyUtil.java
+++ src/main/java/org/minimallycorrect/tickthreading/util/PropertyUtil.java
@@ -9,4 +9,8 @@ public class PropertyUtil {
 	public static boolean get(String name, boolean default_) {
 		return Boolean.parseBoolean(System.getProperty(PROPERTY_PREFIX + name, String.valueOf(default_)));
 	}
+
+	public static int get(String name, int default_) {
+		return Integer.parseInt(System.getProperty(PROPERTY_PREFIX + name, String.valueOf(default_)));
+	}
 }
